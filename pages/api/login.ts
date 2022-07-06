7f5d8a32d0fc93f91e9ebe8ff93cb96be3f657ed
francelino <fmarcilio1989@gmail.com>
2022-07-06 17:40:06 -0300

    adicionando o middleware ao endpoint

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,12 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const endpointLogin = (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { login, senha } = req.body;
     if (login === "admin@admin.com" && senha === "admin") {
-      res.status(200).json({ msg: "Usuário autenticado com sucesso." });
+      return res.status(200).json({ msg: "Usuário autenticado com sucesso." });
     }
     return res.status(405).json({ erro: "Usuário ou Senha não encontrado." });
   }
   return res.status(405).json({ erro: "Metodo informado não é válido." });
 };
+
+export default conectarMongoDB(endpointLogin);
