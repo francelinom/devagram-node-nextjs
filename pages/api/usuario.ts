273e302fa3144d52fd56398454f8971408fdb236
francelino <fmarcilio1989@gmail.com>
2022-07-08 18:16:33 -0300

    inicio da operação de editar usuario

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -3,6 +3,28 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import nc from "next-connect";
+import { upload, uploadImagemCosmic } from "../../services/uploadImagemCosmic";
+
+const handler = nc()
+  .use(upload.single("avatar"))
+  .put(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+      const { userId } = req?.query;
+      const usuario = await UsuarioModel.findById(userId);
+
+      if (!usuario) {
+        return res.status(400).json({ erro: "Usuário não encontrado." });
+      }
+
+      const { nome } = req.body;
+    } catch (e) {
+      console.log(e);
+    }
+    return res
+      .status(400)
+      .json({ erro: "Não foi possível atualizar usuário." });
+  });
 
 const usuarioEndpoint = async (
   req: NextApiRequest,
