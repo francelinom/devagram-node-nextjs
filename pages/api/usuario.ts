65cd953be3f6fb8542140994fa027a79f9048455
francelino <fmarcilio1989@gmail.com>
2022-07-11 14:18:48 -0300

    finalizando o endpoint de alteração do usuário

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -7,8 +7,8 @@ import nc from "next-connect";
 import { upload, uploadImagemCosmic } from "../../services/uploadImagemCosmic";
 
 const handler = nc()
-  .use(upload.single("avatar"))
-  .put(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+  .use(upload.single("file"))
+  .put(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
     try {
       const { userId } = req?.query;
       const usuario = await UsuarioModel.findById(userId);
@@ -18,29 +18,49 @@ const handler = nc()
       }
 
       const { nome } = req.body;
+      if (nome && nome.length > 2) {
+        usuario.nome = nome;
+      }
+      const { file } = req;
+      if (file && file.originalname) {
+        const image = await uploadImagemCosmic(req);
+        if (image && image.media && image.media.url) {
+          usuario.avatar = image.media.url;
+        }
+      }
+      // alterar os dados no DB
+      await UsuarioModel.findByIdAndUpdate({ _id: usuario._id }, usuario);
+      return res.status(200).json({ msg: "Usuário alterado com sucesso" });
     } catch (e) {
       console.log(e);
+      return res
+        .status(400)
+        .json({ erro: "Não foi possível atualizar usuário." + e });
     }
-    return res
-      .status(400)
-      .json({ erro: "Não foi possível atualizar usuário." });
-  });
+  })
+  .get(
+    async (
+      req: NextApiRequest,
+      res: NextApiResponse<RespostaPadraoMsg | any>
+    ) => {
+      try {
+        const { userId } = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+      } catch (e) {
+        console.log(e);
+        return res
+          .status(400)
+          .json({ erro: "Não foi possível obter dados do usuário" });
+      }
+    }
+  );
 
-const usuarioEndpoint = async (
-  req: NextApiRequest,
-  res: NextApiResponse<RespostaPadraoMsg | any>
-) => {
-  try {
-    const { userId } = req?.query;
-    const usuario = await UsuarioModel.findById(userId);
-    usuario.senha = null;
-    return res.status(200).json(usuario);
-  } catch (e) {
-    console.log(e);
-    return res
-      .status(400)
-      .json({ erro: "Não foi possível obter dados do usuário" });
-  }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
 
-export default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
+export default validarTokenJWT(conectarMongoDB(handler));
